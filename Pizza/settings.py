0712bc2a4718182a6f9ed750de1d0ec4fa62707c
rajish <maharjanrajish101@gmail.com>
2022-08-23 15:47:20 +0545

    secret key hidden

--- Pizza/settings.py
+++ Pizza/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t*d%*!0@18hsj%+zrsnw&-(j7p&nm3)c&r+4u_&y78znxn!i)@'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
