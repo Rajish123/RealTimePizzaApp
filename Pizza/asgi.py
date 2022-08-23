d18fb654ef67bb0714eaa1d5dd0194e67c15efef
rajish <maharjanrajish101@gmail.com>
2022-08-23 15:46:58 +0545

    routes for consumer

--- Pizza/asgi.py
+++ Pizza/asgi.py
@@ -13,12 +13,15 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
+from home import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pizza.settings')
 
 # application = get_asgi_application()
 
-ws_pattern = []
+ws_pattern = [
+    path('ws/pizza/<order_id>',consumers.OrderProgress.as_asgi()),
+]
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
